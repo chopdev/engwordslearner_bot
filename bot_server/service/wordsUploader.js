fa7eea055360d2b4651ac340c604f951c5a31110
chopdev <taras.plavin@gmail.com>
2020-10-24 19:13:43 -0700

    update words uploader

--- bot_server/service/wordsUploader.js
+++ bot_server/service/wordsUploader.js
@@ -1,14 +1,26 @@
 const { getWordsFromExcel } = require("../parser/excelParser");
 const { getWordsFromFile } = require("../parser/parser");
 const { DbRepository } = require("../repository/databaseRetriever");
+const { QueueRepository } = require("../repository/queueRepository");
 const config = require("../config/configData");
 
 
-const words = getWordsFromExcel('/tmp/yanilov_words_1.xlsx');
-//const words = getWordsFromFile();
+;; (async () => {
 
-const repo = new DbRepository();
+    const words = getWordsFromExcel('/tmp/taras_words1.xlsx');
+    //const words = getWordsFromExcel('/tmp/yanilov_words_1.xlsx');
+    console.log(words);
 
-for(const word of words) {
-    repo.saveWord(word, config.user_id);
-}
+    console.log("COUNT:" + words.length);
+
+
+    const repo = new DbRepository();
+    const queueRepository = new QueueRepository();
+
+    for(const word of words) {
+        await repo.saveWord(word, config.user_id);
+        await queueRepository.addWord(word.eng)
+    }
+
+})();
+
